e4c4b846618ca8da788e744af666e1f536db27f6
Hayden Dippel <hdippel@wisc.edu>
2025-03-13 23:53:48 -0500

    added logging structs

---
+++ logging.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"time"
+)
+
+type User struct {
+	UserID  string    `gorm:"type:uuid;unique;not null"`
+	Created time.Time `gorm:"type:timestamp;not null"`
+}
+
+type Session struct {
+	User      User      `gorm:"foreignKey:UserID;references:UserID"`
+	SessionID string    `gorm:"type:uuid;unique;not null"`
+	Created   time.Time `gorm:"type:timestamp;not null"`
+}
+
+type Queries struct {
+	Sessions Session   `gorm:"foreignKey:SessionID;references:SessionID"`
+	Gym      string    `gorm:"type:varchar(100);not null;check:Gym IN ('nick', 'bakke')"`
+	Facility string    `gorm:"type:varchar(100);not null"`
+	Created  time.Time `gorm:"type:timestamp;not null"`
+}
