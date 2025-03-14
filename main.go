be2bc113376c24d703c1d063999c2644a32bdcb0
Hayden Dippel <hdippel@wisc.edu>
2025-03-13 20:22:00 -0500

    connect to DB in main

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func main() {
+	initDB()
+
 	r := gin.Default()
 
 	r.Use(middleware)
@@ -17,6 +27,55 @@ func main() {
 	r.GET("/schedule", schedule)
 
 	r.Run(":8000")
+}
+
+func initDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf(".env file not found")
+	}
+
+	// Verfiy .env file has expected variables
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	if host == "" || user == "" || password == "" || dbname == "" || port == "" {
+		log.Fatalf(
+			"One or more of the environment variables was empty or doesn't exist\n"+
+				"\tDB_HOST: \"%s\"\n"+
+				"\tDB_USER: \"%s\"\n"+
+				"\tDB_PASSWORD: \"%s\"\n"+
+				"\tDB_NAME: \"%s\"\n"+
+				"\tDB_PORT: \"%s\"\n",
+			host, user, password, dbname, port,
+		)
+	}
+
+	// Format DSN and get DB object
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Chicago",
+		host, user, password, dbname, port,
+	)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Panicf("Failed to connect to database: %v", err)
+	}
+
+	// Get the underlying SQL DB object
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalf("Failed to get database: %v", err)
+	}
+
+	// Configure connection pool
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	// Auto migrate your models
+	DB.AutoMigrate(&User{}, &Session{}, &Queries{})
 }
 
 func middleware(c *gin.Context) {
